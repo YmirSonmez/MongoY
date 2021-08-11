98037ab9bd42a74e0bb43ba5888abbe1b7e53328
YmirSG <denizsnmz27@gmail.com>
2021-08-11 09:44:22 +0300

    MY

---
+++ src/main/java/me/ymir/mongoy/YObject.java
@@ -0,0 +1,17 @@
+package me.ymir.mongoy;
+
+public class YObject {
+    public String key;
+
+    public YObject(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
