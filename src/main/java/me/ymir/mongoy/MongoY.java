4ff27fce49bd3b04c7329d986a45108a12bd7116
Güney Yılmaz <guneyyilmaz2707@gmail.com>
2023-07-30 13:37:25 +0300

    Added a constructor to connect to MongoDB with a URI

--- src/main/java/me/ymir/mongoy/MongoY.java
+++ src/main/java/me/ymir/mongoy/MongoY.java
@@ -1,15 +1,21 @@
 package me.ymir.mongoy;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
 @SuppressWarnings("unused")
-public class MongoY extends YDataBase{
+public class MongoY extends YDataBase {
     private final MongoClient mongo;
 
-    public MongoY(String host,int port,String dbName) {
+    public MongoY(String host, int port, String dbName) {
         this.mongo = new MongoClient(host, port);
+        init(mongo.getDatabase(dbName));
+    }
+
+    public MongoY(MongoClientURI uri, String dbName) {
+        this.mongo = new MongoClient(uri);
         init(mongo.getDatabase(dbName));
     }
 
@@ -19,14 +25,13 @@ public class MongoY extends YDataBase{
         return this.database.getCollection(collection);
     }
 
-    public YDataBase getAnotherDatabase(String dataBase){
+    public YDataBase getAnotherDatabase(String dataBase) {
         YDataBase db = new YDataBase();
         db.init(mongo.getDatabase(dataBase));
         return db;
     }
 
-    public MongoClient getMongoClient(){
+    public MongoClient getMongoClient() {
         return this.mongo;
     }
-
 }
